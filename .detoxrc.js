f3f866f1c5e9c8355d9fe85dde305492a7b9e593
RajeshKumar Nasit <rajesh.nasit@yahoo.com>
2023-05-21 12:32:24 +0530

    Working andoroid test case simlulator is opened

--- .detoxrc.js
+++ .detoxrc.js
@@ -9,17 +9,59 @@ module.exports = {
       setupTimeout: 120000
     }
   },
+  artifacts: {
+    /* local (per-configuration) section */
+    "plugins": {
+      "instruments": {"enabled": false},
+      "log": {"enabled": true},
+      "uiHierarchy": "enabled",
+      "screenshot": {
+        "shouldTakeAutomaticSnapshots": true,
+        "keepOnlyFailedTestsArtifacts": true,
+        "takeWhen": {
+          "testStart": false,
+          "testDone": true
+        }
+      },
+      "video": {
+        "android": {
+          "bitRate": 4000000
+        },
+        "simulator": {
+          "codec": "hevc"
+        }
+      }
+    }
+  },
   apps: {
-    'ios': {
+    'ios.debug': {
+      binaryPath: "/Users/Ankit/Library/Developer/Xcode/DerivedData/Raven-echnisplcztxcgbcdichyefflysk/Build/Products/Debug-iphonesimulator/Raven.app",
+      build: "xcodebuild -workspace ios/Raven.xcworkspace -configuration debug -scheme Raven -sdk iphonesimulator -derivedDataPath -destination id=F809E87B-AAAD-48AD-8774-3FAED6E91D9A",
       type: 'ios.app',
-      binaryPath: './ios/MyDemoApp.app',
     },
-    'android': {
+    'ios.release': {
+      binaryPath: "",
+      build: "",
+      type: 'ios.app',
+    },
+    'android.debug': {
       type: 'android.apk',
-      binaryPath: './apk/MyDemoApp.apk',
-      testBinaryPath: './apk/MyDemoApp.apk',
-      packageName: 'com.saucelabs.mydemoapp.rn'
-
+      // /Users/Ankit/AndroidStudioProjects/Raven/app/build/intermediates/apk/debug/app-debug.apk
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      // testBinaryPath: 'android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
+      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [
+        8081
+      ]
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: 'apk/app-release.apk',
+      testBinaryPath: 'apk/app-release-androidTest.apk',
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+      reversePorts: [
+        8081
+      ]
     }
   },
   devices: {
@@ -32,28 +74,41 @@ module.exports = {
     attached: {
       type: 'android.attached',
       device: {
-        adbName: '.*' //Any attached devices using usb
+        adbName: '.*'
       }
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a'
+        avdName: 'Pixel_4_XL_API_32'
+        // avdName: 'Pixel_API_28_AOSP'
       }
     }
   },
   configurations: {
-    'ios': {
+    'ios.sim.debug': {
       device: 'simulator',
-      app: 'ios'
+      app: 'ios.debug'
     },
-    'android': {
-      device: 'emulator',
-      app: 'android'
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release'
     },
-    'android.att': {
+    'android.att.debug': {
       device: 'attached',
-      app: 'android.att'
+      app: 'android.debug'
     },
+    'android.att.release': {
+      device: 'attached',
+      app: 'android.release'
+    },
+    'android.emu.debug': {
+      device: 'emulator',
+      app: 'android.debug'
+    },
+    'android.emu.release': {
+      device: 'emulator',
+      app: 'android.release'
+    }
   }
 };
